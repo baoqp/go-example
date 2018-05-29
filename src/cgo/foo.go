f440c319b8f1299e13d902075612e9e758a781ed
baoqp <baoqp@rd.netease.com>
2018-05-29 19:02:52 +0800

    cgo-2

--- src/cgo/foo.go
+++ src/cgo/foo.go
@@ -1,12 +1,15 @@
 package main
 
-// #cgo LDFLAGS: -L ./ -lfoo
+
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include "foo.h"
 import "C"
 import "fmt"
 
+// 参考 https://www.jianshu.com/p/ce97accb1801
+// go build
+// cmd下运行cgo
 func main() {
 	fmt.Println(C.count)
 	C.foo()
